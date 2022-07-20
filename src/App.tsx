ef403738a19c971f8d34d74e639c8bf9645a0744
teuteuf <antoine.teuf@gmail.com>
2022-07-20 22:13:15 +0200

    guess part

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Twemoji } from "@teuteuf/react-emoji-render";
-import { pickRandomMovie } from "./top250movies";
+import { Movie, pickRandomMovie, top250movies } from "./top250movies";
 import { useMemo, useState } from "react";
 import { Picker } from "emoji-mart";
 import "emoji-mart/css/emoji-mart.css";
@@ -8,31 +8,47 @@ import * as utf8 from "utf8";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Flip, toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Route, Routes, useParams, useSearchParams } from "react-router-dom";
+import { Link, Route, Routes, useParams } from "react-router-dom";
+
+interface EmoviToGuess {
+  id: string;
+  emojiText: string;
+}
+
+function MovieCard({ movie }: { movie: Movie }) {
+  return (
+    <div className="flex flex-col items-center">
+      <img className="max-h-64" src={movie.image} alt="Movie's poster" />
+      <p className="text-lg font-bold text-center">{movie.fullTitle}</p>
+    </div>
+  );
+}
+
+function buildShareUrl(emoviToGuess: EmoviToGuess) {
+  const encodedEmovi = encodeURIComponent(
+    base64.encode(utf8.encode(JSON.stringify(emoviToGuess)))
+  );
+  return `${window.location.origin}/guess/${encodedEmovi}`;
+}
 
 function MakeAEmovi() {
   const [movieToGuess, setMovieToGuess] = useState(pickRandomMovie());
   const [emojiText, setEmojiText] = useState("");
   const [validated, setValidated] = useState(false);
 
-  const shareUrl = useMemo(() => {
-    const encodedEmovi = base64.encode(
-      utf8.encode(JSON.stringify({ id: movieToGuess.id, emojiText }))
-    );
-    return `${window.location.origin}/emovi/${encodedEmovi}`;
-  }, [emojiText, movieToGuess.id]);
+  const shareUrl = useMemo(
+    () => buildShareUrl({ id: movieToGuess.id, emojiText }),
+    [emojiText, movieToGuess.id]
+  );
+
+  const shareText = useMemo(() => {
+    return ["#Emovi 🎬", "Guess this movie:", emojiText, shareUrl].join("\n");
+  }, [emojiText, shareUrl]);
 
   return (
     <div>
       <div className="flex flex-col items-center gap-2">
-        <img
-          className="max-h-64"
-          src={movieToGuess.image}
-          alt="Movie's poster"
-        />
-        <p className="text-lg font-bold text-center">
-          {movieToGuess.fullTitle}
-        </p>
+        <MovieCard movie={movieToGuess} />
         {!validated && (
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
@@ -46,7 +62,7 @@ function MakeAEmovi() {
       </div>
       <div className="flex flex-col gap-1 items-center">
         <div className="flex flex-col gap-2 justify-center items-center grow shrink-0 basis-24">
-          <p className="text-xl">
+          <p className="text-xl text-center">
             {emojiText ? (
               <Twemoji
                 text={emojiText}
@@ -87,7 +103,7 @@ function MakeAEmovi() {
         ) : (
           <div>
             <CopyToClipboard
-              text={shareUrl}
+              text={shareText}
               onCopy={() => toast("Emovi copied to clipboard")}
               options={{
                 format: "text/plain",
@@ -106,16 +122,112 @@ function MakeAEmovi() {
 
 function GuessAEmovi() {
   const { emovi: encodedEmovi } = useParams<{ emovi: string }>();
-  console.log(JSON.parse(utf8.decode(base64.decode(encodedEmovi!))));
-  const emoviToGuess: { id: string; emojiText: string } | undefined =
-    encodedEmovi
-      ? JSON.parse(utf8.decode(base64.decode(encodedEmovi)))
-      : undefined;
+  const emoviToGuess: EmoviToGuess | undefined = useMemo(
+    () =>
+      encodedEmovi
+        ? JSON.parse(
+            utf8.decode(base64.decode(decodeURIComponent(encodedEmovi)))
+          )
+        : undefined,
+    [encodedEmovi]
+  );
+
+  const movieToGuess = useMemo(() => {
+    return emoviToGuess && top250movies.find((m) => m.id === emoviToGuess.id);
+  }, [emoviToGuess]);
+
+  const movieChoices = useMemo(() => {
+    const otherMovies = top250movies.filter((m) => m.id !== movieToGuess?.id);
+    const nineRandomPickedMovies = [...otherMovies]
+      .sort(() => 0.5 - Math.random())
+      .slice(0, 9);
+
+    if (!movieToGuess) {
+      return [];
+    }
+
+    return [movieToGuess, ...nineRandomPickedMovies].sort(
+      () => 0.5 - Math.random()
+    );
+  }, [movieToGuess]);
+
+  const [invalidGuessIds, setInvalidGuessIds] = useState<string[]>([]);
+  const [movieGuessed, setMovieGuessed] = useState<boolean>(false);
+  const handleTryGuess = (movieId: string) => {
+    if (emoviToGuess && emoviToGuess.id === movieId) {
+      setMovieGuessed(true);
+      toast.success("You guessed it!");
+    } else {
+      setInvalidGuessIds((prev) => [...prev, movieId]);
+      toast.error("Wrong movie!");
+    }
+  };
+
+  const shareText = useMemo(() => {
+    if (!emoviToGuess || !movieToGuess) {
+      return "";
+    }
+
+    return [
+      "#Emovi 🎬",
+      emoviToGuess.emojiText,
+      `Guessed in ${invalidGuessIds.length + 1} guesses!`,
+      buildShareUrl(emoviToGuess),
+    ].join("\n");
+  }, [emoviToGuess, invalidGuessIds.length, movieToGuess]);
 
   return (
-    <div>
-      <p>GUESS!</p>
-      {emoviToGuess && <p>{emoviToGuess.emojiText}</p>}
+    <div className="flex flex-col items-center gap-2">
+      <p className="text-lg font-bold text-center">Guess this movie:</p>
+      {emoviToGuess && (
+        <p className="text-xl text-center">
+          <Twemoji
+            text={emoviToGuess.emojiText}
+            options={{ className: "inline-block" }}
+          />
+        </p>
+      )}
+      {!movieGuessed ? (
+        <div className="flex flex-col items-center gap-1">
+          {movieChoices.map((movie) => (
+            <button
+              key={movie.id}
+              disabled={invalidGuessIds.includes(movie.id)}
+              className="bg-blue-500 hover:bg-blue-700 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded w-full"
+              onClick={() => handleTryGuess(movie.id)}
+            >
+              {movie.fullTitle}
+            </button>
+          ))}
+        </div>
+      ) : (
+        <div className="flex flex-col gap-2 text-center">
+          {movieToGuess && <MovieCard movie={movieToGuess} />}
+          <div>
+            <p>Well done! You guessed the movie!</p>
+            <p>In {invalidGuessIds.length + 1} guesses!</p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <Link
+              to="/"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+            >
+              Create your Emovie!
+            </Link>
+            <CopyToClipboard
+              text={shareText}
+              onCopy={() => toast("Result copied to clipboard")}
+              options={{
+                format: "text/plain",
+              }}
+            >
+              <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded w-full">
+                Share your result!
+              </button>
+            </CopyToClipboard>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
@@ -139,7 +251,7 @@ function App() {
         </header>
         <Routes>
           <Route path="/" element={<MakeAEmovi />} />
-          <Route path="/emovi/:emovi" element={<GuessAEmovi />} />
+          <Route path="/guess/:emovi" element={<GuessAEmovi />} />
         </Routes>
       </div>
     </div>
