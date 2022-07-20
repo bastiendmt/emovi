fd43a6674c831d2e49af0737df13c7120188c2d8
teuteuf <antoine.teuf@gmail.com>
2022-07-20 22:13:15 +0200

    create a emovi

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Twemoji } from "@teuteuf/react-emoji-render";
+import { pickRandomMovie } from "./top250movies";
+import { useState } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
+import * as base64 from "base-64";
+import * as utf8 from "utf8";
 
 function App() {
+  const [movieToGuess, setMovieToGuess] = useState(pickRandomMovie());
+  const [emojiText, setEmojiText] = useState("");
+  const [validated, setValidated] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="flex flex-col gap-2 mb-2">
+      <header className="text-2xl font-extrabold text-center p-4">
+        <Twemoji text="🎬 EMOVI 🎥" options={{ className: "inline-block" }} />
       </header>
+      <div className="flex flex-col items-center gap-2">
+        <img
+          className="max-h-64"
+          src={movieToGuess.image}
+          alt="Movie's poster"
+        />
+        <p className="text-lg font-bold">{movieToGuess.fullTitle}</p>
+        {!validated && (
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => {
+              setMovieToGuess(pickRandomMovie());
+            }}
+          >
+            Pick another movie...
+          </button>
+        )}
+      </div>
+      <div className="flex flex-col gap-1 items-center">
+        <p className="text-xl">
+          {emojiText ? (
+            <Twemoji text={emojiText} options={{ className: "inline-block" }} />
+          ) : (
+            <span> Describe movie with emoji!</span>
+          )}
+        </p>
+        {emojiText && !validated && (
+          <div className="flex gap-2">
+            <button
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                setEmojiText("");
+              }}
+            >
+              Clear!
+            </button>
+            <button
+              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+              onClick={() => setValidated(true)}
+            >
+              Validate!
+            </button>
+          </div>
+        )}
+        {!validated ? (
+          <Picker
+            showSkinTones={false}
+            showPreview
+            onSelect={(e: any) => {
+              setEmojiText((prev) => prev + e.native);
+            }}
+          />
+        ) : (
+          utf8.decode(
+            base64.decode(
+              base64.encode(
+                utf8.encode(JSON.stringify({ id: movieToGuess.id, emojiText }))
+              )
+            )
+          )
+        )}
+      </div>
     </div>
   );
 }
